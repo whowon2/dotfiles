7171c695d11720276fe3179c46eb858ddac5d250
juaniwk3 <juaniwk3@gmail.com>
2023-12-21 00:13:49 -0300

    alacritty font

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,15 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 alias p3="python3"
+alias ct="cargo test"
+alias cr="cargo run"
+alias pd="pnpm dev"
+alias pps="pnpm prisma studio"
+alias nv="nvim"
+
+# pnpm
+set -gx PNPM_HOME "/home/rusarc/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
